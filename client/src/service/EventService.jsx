4e5eed11b74cb68b92024976c2cf272dea91fbfb
florienborg <flornbg014@gmail.com>
2023-09-14 15:37:59 +0200

    estructura de carpetas y archivos

---
+++ client/src/service/EventService.jsx
@@ -0,0 +1,34 @@
+// import axios from 'axios';
+// axios.defaults.baseURL = 'http://localhost:8000/';
+// axios.defaults.headers.post['Content-Type'] = 'application/json';
+// axios.defaults.headers.post['Accept'] = 'application/json';
+// axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
+// axios.defaults.withCredentials = true;
+
+
+// axios.interceptors.request.use(function (res){
+//     const token = localStorage.getItem('auth_token');
+//     res.headers.Authorization = token ? `Bearer ${token}`: '';
+//         return res
+// }
+// );
+
+
+// export const DateService =  () => {
+//   const url = '/api';
+  
+//   const getAllDates = () => {
+//     const response =  axios.get(url)
+//     return response
+//   }
+
+//   const createDates = (date) => {
+//     const response = axios.post (`${url}/create`, date);
+//     return response
+  
+//   }
+
+
+//   return {getAllDates, createDates};
+// }
