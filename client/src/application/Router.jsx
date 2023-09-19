3dc4e550a766ffa328e855ba0109684aef2ca29e
Jeaneth Sanchez Nuñez <jeaneth.sanchez@gmail.com>
2023-09-19 20:12:28 +0200

    dashboard events

--- client/src/application/Router.jsx
+++ client/src/application/Router.jsx
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import HomePage from '../pages/home/HomePage';
 import RegisterPage from '../pages/register/RegisterPage';
-import LoginPage from '../pages/login/LoginPage';
+import LoginPage from '../pages/login/LoginPage'
+import Dashboard from '../pages/admin/Dashboard';
 
 const router = createBrowserRouter([
   { 
@@ -15,6 +16,11 @@ const router = createBrowserRouter([
   {
     path: '/login',
     element: <LoginPage />
+  },
+
+  {
+    path: '/dashboard',
+    element: <Dashboard />
   }
 ])
 
---
+++ client/src/pages/admin/Dashboard.jsx
@@ -0,0 +1,32 @@
+import { EventService } from '../../service/EventService';
+import React, { useState, useEffect } from 'react';
+import Events from "../../components/admin/Events";
+
+
+function Dashboard() {
+
+  const [events, setEvents] = useState ([]); 
+  
+
+useEffect(() =>{
+  const api = EventService();
+   api.getAllEvents().then(res => {
+      setEvents(res.data);
+   })
+
+}, []);
+ 
+ 
+
+  return (
+    <>
+      {
+        events.map((event, index) => (
+        <Events key={index} event = {event}/>
+        ))
+      }
+    </>
+  )
+} 
+
+export default Dashboard;
