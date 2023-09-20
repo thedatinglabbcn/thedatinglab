7179807b927148cd692d53b694ddb4a1fd5a87f5
dkhurlop <denisekhurlopian@gmail.com>
2023-09-20 16:25:43 +0200

    home page/logout/navbar

--- client/src/components/forms/LoginForm.jsx
+++ client/src/components/forms/LoginForm.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-// import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Forms.css';
 import { AuthService } from '../../service/AuthService';
@@ -25,15 +24,16 @@ const LoginForm = () => {
       auth.login(formData).then(res => {
         const { token } = res.data;
 
-        localStorage.setItem('token', token);
-        
-
+        localStorage.setItem('auth_token', token);
+      
         Swal.fire({
           title: '¡Inicio de sesión exitoso!',
           text: '¡Bienvenido!',
           icon: 'success',
         });
-        console.log(res);
+
+        
+        //console.log(res);
       }).catch(err => {
         Swal.fire({
           title: '¡Error!',
@@ -43,23 +43,9 @@ const LoginForm = () => {
         console.log(err);
       });
 
-
-
-      // Aquí puedes realizar acciones con los datos del formulario, como enviarlos a un servidor para la autenticación.
-      console.log(formData);
+      //console.log(formData);
     };
-  
-  const handleLogout = (e) => {
-      e.preventDefault();
-      const auth = AuthService();
-      auth.logout().then(res => {
-        localStorage.removeItem('auth_token');
-        console.log(res);
-      }).catch(err => {
-        console.log(err);
-      });
-  };
-
+ 
     return (
       <div className="container">
         <h1 className='form-title'>Iniciar Sesión</h1>
@@ -98,9 +84,7 @@ const LoginForm = () => {
             Ingresar
           </button>
         </form>
-        <button onClick={handleLogout} className="button-send">
-            logout
-          </button>
+        
         <p className='form-help'>
           ¿Has olvidado la contraseña? <a href="#">Toca aquí</a>
         </p>
--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -1,6 +1,8 @@
 import { EventService } from '../../service/EventService';
 import React, { useState, useEffect } from 'react';
 import EventCard from "../../components/eventCard/EventCard";
+import Navbar from '../../components/navbar/Navbar';
+import './HomePage.css';
 
 
 function HomePage() {
@@ -20,11 +22,39 @@ useEffect(() =>{
 
   return (
     <>
-      {
-        events.map((event, index) => (
+      <Navbar />
+      <section className="hero">
+        <div className="hero-content">
+          <h1>Donde los sentidos encuentran el amor</h1>
+          <p>Creemos en la compatibilidad emocional como la base de relaciones sólidas.</p>
+          <button type="button" className="button-start"  onClick={() => window.location.href = '/'}>
+              ¿Te apuntas?
+            </button>
+        </div>
+      </section>
+
+      <section className="how-it-works">
+        <div className="how-it-works-content">
+          <h2>¿Cómo funciona?</h2>
+          <ol>
+            <li>Completa el formulario de compatibilidad emocional.</li>
+            <li>Recibe tus match personalizados.</li>
+            <li>Únete a nuestros emocionantes eventos basados en tus intereses.</li>
+            <li>Pagarás por asistir a los eventos y así conocer a tus match.</li>
+            <li>Disfruta de citas significativas y experiencias sensoriales.</li>
+          </ol>
+          <button type="button" className="button-start"  onClick={() => window.location.href = '/register'}>
+              Empezar ahora
+            </button>
+        </div>
+      </section>
+      <section id="event-section">
+        <h1 className='title-Events'>Experiencias</h1>
+      { events.map((event, index) => (
         <EventCard key={index} event = {event}/>
         ))
       }
+      </section>
     </>
   )
 } 
