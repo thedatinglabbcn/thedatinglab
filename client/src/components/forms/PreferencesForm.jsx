f69e2e866a8b79f61e11ca3f1f4a6f98ff3c8a94
rosa-rubio <rosamrubio@gmail.com>
2023-09-23 19:08:42 +0200

    Preferences radio boxes changed to buttons

--- client/src/components/forms/PreferencesForm.jsx
+++ client/src/components/forms/PreferencesForm.jsx
@@ -55,240 +55,246 @@ const PreferencesForm = () => {
             <label className="form-label">¿Con qué género te identificas?</label>
             <div>
               <input type="radio"
-                className='form-radio'
+                className='btn-check'
+                autoComplete='off'
                 id="hombre"
                 name="gender"
                 value="Hombre"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="hombre">Hombre</label>
+              <label className="btn btn-outline-danger" htmlFor="hombre">Hombre</label>
             </div>
             <div>
-              <input
-                type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="mujer"
                 name="gender"
                 value="Mujer"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="mujer">Mujer</label>
+              <label className="btn btn-outline-danger" htmlFor="mujer">Mujer</label>
             </div>
             <div>
-              <input
-                type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="otro"
                 name="gender"
                 value="Otro"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="otro">Otro</label>
+              <label className="btn btn-outline-danger" htmlFor="otro">Otro</label>
             </div>
           </div>
           <div className="mb-4">
             <label className="form-label">Buscas...</label>
             <div>
-              <input type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="buscaHombre"
                 name="looksFor"
                 value="Hombre"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="buscaHombre">Hombre</label>
+              <label className="btn btn-outline-danger" htmlFor="buscaHombre">Hombre</label>
             </div>
             <div>
-              <input
-                type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="buscaMujer"
                 name="looksFor"
                 value="Mujer"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="buscaMujer">Mujer</label>
+              <label className="btn btn-outline-danger" htmlFor="buscaMujer">Mujer</label>
             </div>
             <div>
-              <input
-                type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="buscaOtro"
                 name="looksFor"
                 value="Otro"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="buscaOtro">Otro</label>
+              <label className="btn btn-outline-danger" htmlFor="buscaOtro">Otro</label>
             </div>
           </div>
           <div className="mb-4">
             <label>Eres más de...</label>
             <div>
-              <input
-                type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="netflix"
                 name="preferences1"
                 value="Netflix"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="netflix">Netflix y mi sofá</label>
+              <label className="btn btn-outline-danger" htmlFor="netflix">Netflix y mi sofá</label>
             </div>
             <div>
-              <input
-                type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="eventos"
                 name="preferences1"
                 value="Eventos"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="eventos">Eventos, restaurantes, copeo</label>
+              <label className="btn btn-outline-danger" htmlFor="eventos">Eventos, restaurantes, copeo</label>
             </div>
             <div>
-              <input
-                type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="deporte"
                 name="preferences1"
                 value="Deporte"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="deporte">Gimnasio, deporte y aire libre</label>
+              <label className="btn btn-outline-danger" htmlFor="deporte">Gimnasio, deporte y aire libre</label>
             </div>
             <div>
-              <input
-                type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="escapadas"
                 name="preferences1"
                 value="Escapadas"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="escapadas">Escapadas, playa y montaña</label>
+              <label className="btn btn-outline-danger" htmlFor="escapadas">Escapadas, playa y montaña</label>
             </div>
             <div>
-              <input
-                type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="todas"
                 name="preferences1"
                 value="Todas"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="todas">Todas las anteriores</label>
+              <label className="btn btn-outline-danger" htmlFor="todas">Todas las anteriores</label>
             </div>
             <div>
-              <input
-                type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="otras"
                 name="preferences1"
                 value="Otras"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="otras">Otras que te invito a descubrir</label>
+              <label className="btn btn-outline-danger" htmlFor="otras">Otras que te invito a descubrir</label>
             </div>
           </div>
           <div className="mb-4">
             <label>Eres más de...</label>
             <div>
-              <input
-                type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="alcohol"
                 name="preferences2"
                 value="Alcohol"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="alcohol">Cata de vinos, cerveza, vermut... o lo que se tercie</label>
+              <label className="btn btn-outline-danger" htmlFor="alcohol">Cata de vinos, cerveza, vermut... o lo que se tercie</label>
             </div>
             <div>
-              <input
-                type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="infusiones"
                 name="preferences2"
                 value="Infusiones"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="infusiones">Café, chai latte, mate, infusiones varias...</label>
+              <label className="btn btn-outline-danger" htmlFor="infusiones">Café, chai latte, mate, infusiones varias...</label>
             </div>
             <div>
-              <input
-                type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="noalcohol"
                 name="preferences2"
                 value="NoAlcohol"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="noalcohol">Agua, zumos naturales, batidos detox</label>
+              <label className="btn btn-outline-danger" htmlFor="noalcohol">Agua, zumos naturales, batidos detox</label>
             </div>
             <div>
-              <input
-                type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="según"
                 name="preferences2"
                 value="Según"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="según">Según el momento o casi todas las anteriores</label>
+              <label className="btn btn-outline-danger" htmlFor="según">Según el momento o casi todas las anteriores</label>
             </div>
             <div>
-              <input
-                type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="ninguna"
                 name="preferences2"
                 value="Ninguna"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="ninguna">Ninguna, no ingiero líquidos</label>
+              <label className="btn btn-outline-danger" htmlFor="ninguna">Ninguna, no ingiero líquidos</label>
             </div>
           </div>
           <div className="mb-4">
             <label className="form-label">¿Prefieres los gatos o perros?</label>
             <div>
-              <input type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="gatos"
                 name="catsDogs"
                 value="Gatos"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="gatos">Gatos</label>
+              <label className="btn btn-outline-danger" htmlFor="gatos">Gatos</label>
             </div>
             <div>
-              <input type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="perros"
                 name="catsDogs"
                 value="Perros"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="perros">Perros</label>
+              <label className="btn btn-outline-danger" htmlFor="perros">Perros</label>
             </div>
             <div>
-              <input type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="todos"
                 name="catsDogs"
                 value="Todos"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="todos">Todos los animales</label>
+              <label className="btn btn-outline-danger" htmlFor="todos">Todos los animales</label>
             </div>
             <div>
-              <input type="radio"
-                className='form-radio'
+            <input type="radio"
+                className='btn-check'
+                autoComplete='off'
                 id="deAmigos"
                 name="catsDogs"
                 value="DeAmigos"
                 onChange={handleOnChange}
               />
-              <label className="form-label" htmlFor="deAmigos">Me gustan los de mis amig@s</label>
+              <label className="btn btn-outline-danger" htmlFor="deAmigos">Me gustan los de mis amig@s</label>
             </div>
           </div>
           <p className='form-text'>
