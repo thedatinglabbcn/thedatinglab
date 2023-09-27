a76493beb9f62e21a06f14d25c49f37308c9c4c5
florienborg <flornbg014@gmail.com>
2023-09-27 11:30:16 +0200

    match card slider

--- client/src/components/matchCard/MatchCard.jsx
+++ client/src/components/matchCard/MatchCard.jsx
@@ -4,12 +4,14 @@ import Navbar from '../navbar/Navbar';
 import Footer from '../footer/Footer';
 import { MatchingService } from '../../service/MatchingService';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
 
 function MatchCard() {
   const [matchingUsers, setMatchingUsers] = useState([]);
   const [error, setError] = useState(null);
-  const [noPreferencesError, setNoPreferencesError] = useState(false);
-  const [noMatchesError, setNoMatchesError] = useState(false);
+  const [currentMatchIndex, setCurrentMatchIndex] = useState(0);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -18,26 +20,33 @@ function MatchCard() {
         const matches = MatchingService();
         const response = await matches.getAllMatches();
         console.log(response.data);
-  
+
         if (Array.isArray(response.data.matches)) {
           setMatchingUsers(response.data.matches);
         } else {
           setError('La respuesta no es un arreglo válido');
         }
       } catch (error) {
-        console.log(error);
-        if (error.response.status === 404 && error.response.data.type === 'preferences') {
-          setNoPreferencesError(true);
-        } else if (error.response.status === 404 && error.response.data.type === 'matches') {
-          setNoMatchesError(true);
-        } else {
-          setError(error.response.data.message);
-        }
+        console.error('Error al cargar los matches:', error);
       }
     };
 
     fetchData();
-  }, []); 
+  }, []);
+ 
+
+  const navigateToPreviousMatch = () => {
+    if (currentMatchIndex > 0) {
+      setCurrentMatchIndex(currentMatchIndex - 1);
+    }
+  };
+
+  const navigateToNextMatch = () => {
+    if (currentMatchIndex < matchingUsers.length - 1) {
+      setCurrentMatchIndex(currentMatchIndex + 1);
+    }
+  };
+  console.log(navigateToNextMatch)
 
   return (
     <div>
@@ -45,50 +54,37 @@ function MatchCard() {
       <div className='match-container'>
         <h1 className='match-title'>¡Tus matches!</h1>
         <center>
-        {noPreferencesError && (
-  <p className="card-text text-center text-danger">
-    Debes completar el formulario para poder ver tus matches.
-    <p><button
-      type="button"
-      className="button-cancel"
-      style={{ marginTop: '10px' }}
-      onClick={() => navigate('/preferences')}
-    >
-      Formulario
-    </button></p>
-  </p>
-)}
-
-{noMatchesError && (
-  <p className="card-text text-center text-danger">
-    Sin coincidencias por ahora... ¡Vuelve a comprobarlo más tarde!
-    <p><button
-      type="button"
-      className="button-cancel"
-      style={{ marginTop: '10px' }}
-      onClick={() => navigate('/')}
-    >
-      Eventos
-    </button></p>
-  </p>
-)}
-          {matchingUsers.map((user) => (
-            <div className="match-profile" style={{ width: '18rem'}} key={user.id}>
-              <div className="card-img-top">    
-                <center><img src={`http://localhost:8000/storage/${user.image}`} className="rounded-circle" alt={`Tu match: ${user.name}`} /></center>
-              </div>
-              <div className="match-body">
-                <h5 className="card-title text-center">{user.name}</h5>
-                <h6 className="card-text text-center text-muted">Coincidencia: {user.matchingPercentage}%</h6>
-                <p className="card-text text-center"><small>{user.description}</small></p>
-              </div>
+          <div className="match-carousel-container">
+            <FontAwesomeIcon
+              icon={faArrowLeft}
+              className="arrow-icon-left"
+              onClick={navigateToPreviousMatch}
+            />
+            <div className="match-carousel">
+              {matchingUsers.map((user) => (
+                <div className="match-card">
+                  <img
+                    src={`http://localhost:8000/storage/${user.image}`}
+                    className="rounded-circle"
+                    alt={`Tu match: ${user.name}`}
+                  />
+                  <h5 className="card-title text-center">{user.name}</h5>
+                  <h6 className="card-text text-center text-muted">Coincidencia: {user.matchingPercentage}%</h6>
+                  <p className="card-text text-center"><small>{user.description}</small></p>
+                </div>
+              ))}
             </div>
-          ))}
+            <FontAwesomeIcon
+              icon={faArrowRight}
+              className="arrow-icon-right"
+              onClick={navigateToNextMatch}
+            />
+          </div>
         </center>
       </div>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );  
 }
 
 export default MatchCard;
