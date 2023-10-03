26e34ffc20b7c5734176d59007bb924cd4b608d4
florienborg <flornbg014@gmail.com>
2023-10-03 22:46:16 +0200

    faqs pagina/css y en menuhamburguesa

--- client/src/application/Router.jsx
+++ client/src/application/Router.jsx
@@ -6,7 +6,6 @@ import EditForm from '../components/admin/EditForm';
 import LoginPage from '../pages/login/LoginPage'
 import ProfileFormPage from '../pages/register/ProfileFormPage';
 import PreferencesPage from '../pages/register/PreferencesPage';
-import ProfileEditForm from '../components/forms/ProfileEditForm';
 import ProfilePage from '../pages/profile/ProfilePage'
 import Dashboard from '../pages/admin/Dashboard';
 import DashboardEvents from '../pages/admin/DashboardEvents';
@@ -15,6 +14,7 @@ import AdminLoginPage from '../pages/admin/AdminLoginPage';
 import EventsPage from '../pages/events/EventsPage';
 import EventDetail from '../pages/events/EventDetail';
 import PaymentPage from '../pages/payment/PaymentPage';
+import Faqs from '../pages/faqs/Faqs';
 
 
 const router = createBrowserRouter([
@@ -58,7 +58,6 @@ const router = createBrowserRouter([
     path: '/dashboard',
     element: <Dashboard />
   },
-
   {
     path: '/dashboard/events',
     element: <DashboardEvents />
@@ -74,6 +73,10 @@ const router = createBrowserRouter([
   {
     path: '/event/:eventId',
     element: <EventDetail />
+  },
+  {
+    path: '/faqs',
+    element: <Faqs />
   },
   {
     path: '/payment/:eventId',
