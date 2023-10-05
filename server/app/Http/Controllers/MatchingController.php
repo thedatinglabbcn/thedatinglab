13ef0bb6e8fdd6dfbd1b2ee26d015cfd99d391c9
rosa-rubio <rosamrubio@gmail.com>
2023-10-05 22:40:24 +0200

    MatchingController refactorized

--- server/app/Http/Controllers/MatchingController.php
+++ server/app/Http/Controllers/MatchingController.php
@@ -2,12 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
-use App\Models\Preference;
-use App\Models\Profile;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\Auth;
 
 class MatchingController extends Controller
@@ -17,30 +12,14 @@ class MatchingController extends Controller
         try {
             $user = Auth::user();
 
+            $userPreference = $user->preference; 
+
             if (!$user->preference) {
                 return response()->json(['type' => 'preferences'], 404);
             }
 
-            $userPreference = $user->preference; 
+            $weights = json_decode(file_get_contents(resource_path('config/weights.json')), true);
 
-            $userGender = $userPreference->gender;
-            $userLooksFor = $userPreference->looksFor;
-            $userAgeRange = $userPreference->ageRange;
-            
-            $weights = [
-                'gender' => 0,
-                'looksFor' => 0,
-                'birthdate' => 0,
-                'ageRange' => 0,
-                'hasChildren' => 0,
-                'datesParents' => 2,
-                'wantsFamily' => 2,
-                'sexoAffective' => 2,
-                'heartState' => 1,
-                'preferences1' => 1,
-                'preferences2' => 0.5,
-                'catsDogs' => 0.5,
-            ];
 
             $matches = User::findMatchesForUser($user);
 
@@ -71,11 +50,9 @@ class MatchingController extends Controller
 
     private function getPreferenceFields($userPreference)
     {
-        // Obtén dinámicamente la lista de campos de preferencias disponibles
         $fields = array_keys($userPreference->getAttributes());
 
-        // Elimina los campos que no quieres incluir en el cálculo
-        $fieldsToExclude = ['id', 'created_at', 'updated_at', 'gender', 'looksFor', 'birthdate', 'ageRange', 'hasChildren', 'datesParents'];
+        $fieldsToExclude = ['id', 'created_at', 'updated_at', 'gender', 'looksFor', 'birthdate', 'ageRange', 'hasChildren'];
         $fields = array_diff($fields, $fieldsToExclude);
 
         return $fields;
@@ -83,9 +60,7 @@ class MatchingController extends Controller
 
     private function calculateMatchingPercentage($userPreference, $matchPreference, $weights)
 {
-    // $totalFields = count($this->getPreferenceFields($userPreference));
     $matchingFields = 0;
-
 
     foreach ($this->getPreferenceFields($userPreference) as $field) {
         if ($userPreference->$field === $matchPreference->$field) {
